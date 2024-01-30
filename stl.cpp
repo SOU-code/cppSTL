e9808356ae3c173621f50e34f7203d7fceb518ff
tech-souvik <officialsouvikmaji@gmail.com>
2024-01-30 20:10:02 +0530

    Deque Concepts

--- stl.cpp
+++ stl.cpp
@@ -85,9 +85,27 @@ void explainList(){
     //rest of the concept of vector
     //begin,end,rbegin,rend,clear,insert,size,swap
 }
+//Deque Concepts
+void explainDeque(){
+    deque<int> dq;
+    dq.push_back(2);
+    dq.emplace_back(1);
+    dq.push_front(4);
+    dq.emplace_front(3); //{3,4,2,1}
+
+    dq.pop_back(); //{3,4,1}
+    dq.pop_front(); //{4,1}
+
+    dq.back();
+    dq.front();
+    //rest of the concept of vector
+    //begin,end,rbegin,rend,clear,insert,size,swap
+}
+
 int main(){
     explainPair(); //Pair Concepts
     explainVector(); //Vector Concepts
     explainList(); //list concepts
+    explainDeque(); //Deque concepts
     return 0;
 }
